c194595d21cc197b75acf9d0bdbf45f9db3d03fc
purgz <henbrooks6@gmail.com>
2022-10-19 13:50:45 +0100

    added geolocation api to find local city name and then find weather info

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-async function getLatLong(){
-    let name = "bicester"
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q={${name}}&limit={limit}&appid={f7555cc5afd733fa2e73dee02f9ca7b2}`,{mode:"cors"})
-    const locationData = await response.json();
-    console.log(locationData) 
+let localLat;
+let localLon;
 
-async function getWeatherInfo(){
+async function getWeatherInfo(location){
 
-    const locationInfo = getLatLong()
-    const lat = locationInfo.lat;
-    const lon = locationInfo.lon;
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${lon}}&appid={f7555cc5afd733fa2e73dee02f9ca7b2}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f7555cc5afd733fa2e73dee02f9ca7b2`,
+    {
+        mode:"cors"
+    })
     const weatherData = await response.json();
     console.log(weatherData);
 }
 
-getWeatherInfo(); //need to wait for api key to activate
+function getPosPromise(){
+    return new Promise(function(resolve,reject){
+        navigator.geolocation.getCurrentPosition(
+            (position) => resolve(position),
+            (error) => reject(error)
+        )
+    })
+}
+
+async function getLocation(){
+    return await getPosPromise();
+}
+
+async function getCityName(){
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${localLat}&lon=${localLon}&appid=f7555cc5afd733fa2e73dee02f9ca7b2`)
+    const locationData = await response.json();
+    return locationData[0].name;
+}
+
+
+getWeatherInfo("bicester");
+
+
+const localWeatherBtn = document.querySelector("#use-local");
+const locationInput = document.querySelector("#enter-location");
+const locationForm = document.querySelector("#location-form")
+
+localWeatherBtn.addEventListener("click",async e=>{
+
+    let location = await getLocation();
+    localLat = location.coords.latitude;
+    localLon = location.coords.longitude;
+    
+    getWeatherInfo(await getCityName())
+})
+
+locationForm.addEventListener("submit",async e=>{
+    e.preventDefault();
+    let location = e.target[0].value
+    await getWeatherInfo(location);
+})
