142ccfe6eea001fd1898719cc0c60cdec167b7fd
purgz <henbrooks6@gmail.com>
2023-03-11 14:08:41 +0000

    simple weather api app, no very basic styling

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 let localLat;
 let localLon;
+let weatherInfo;
+const weatherIcon = document.querySelector("#weather-icon");
+const description = document.querySelector("#description");
+const details = document.querySelector("#details");
+const temperature = document.querySelector("#temperature");
+const locationName = document.querySelector("#location-name");
 
 async function getWeatherInfo(location){
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f7555cc5afd733fa2e73dee02f9ca7b2`,
-    {
-        mode:"cors"
-    })
-    const weatherData = await response.json();
-    console.log(weatherData);
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f7555cc5afd733fa2e73dee02f9ca7b2`,
+        {
+            mode:"cors"
+        })
+        const weatherData = await response.json();
+        
+        console.log(weatherData);
+      
+        processJson(await weatherData);
+    } catch(error){
+        console.log(error);
+    }
+
 }
 
 function getPosPromise(){
@@ -30,10 +44,6 @@ async function getCityName(){
     return locationData[0].name;
 }
 
-
-getWeatherInfo("bicester");
-
-
 const localWeatherBtn = document.querySelector("#use-local");
 const locationInput = document.querySelector("#enter-location");
 const locationForm = document.querySelector("#location-form")
@@ -44,7 +54,7 @@ localWeatherBtn.addEventListener("click",async e=>{
     localLat = location.coords.latitude;
     localLon = location.coords.longitude;
     
-    getWeatherInfo(await getCityName())
+    getWeatherInfo(await getCityName());
 })
 
 locationForm.addEventListener("submit",async e=>{
@@ -52,3 +62,26 @@ locationForm.addEventListener("submit",async e=>{
     let location = e.target[0].value
     await getWeatherInfo(location);
 })
+
+class WeatherInfo{
+
+    constructor(data){
+       
+        this.temperature = Math.round (data.main.temp - 273.15);
+        this.description = data.weather[0].main;
+        this.descriptionDetails = data.weather[0].description;
+        this.weatherImg = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        this.locationName = data.name;
+    }
+}
+
+function processJson(data){
+
+    weatherInfo = new WeatherInfo(data);
+   
+    weatherIcon.src = weatherInfo.weatherImg;
+    temperature.textContent = weatherInfo.temperature;
+    description.textContent = weatherInfo.description;
+    details.textContent = weatherInfo.descriptionDetails;
+    locationName.textContent = weatherInfo.locationName;
+}
